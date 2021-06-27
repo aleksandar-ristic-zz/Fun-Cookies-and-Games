e8501eabbd1ea34916b1f2cc7eabac6496fff70b
aleksandar-ristic <aristic665@gmail.com>
2021-06-27 23:55:27 +0200

    Added func to add event

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,3 +1,5 @@
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
@@ -18,9 +20,29 @@ export default function AddEventPage() {
 	})
 	const router = useRouter()
 
-	const handleSubmit = e => {
+	const handleSubmit = async e => {
 		e.preventDefault()
-		console.log('submitted')
+
+		const hasEmptyFields = Object.values(values).some(element => element === '')
+
+		if (hasEmptyFields) {
+			toast.error('Please fill in all event fields')
+		}
+
+		const res = await fetch(`${API_URL}/events`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(values)
+		})
+
+		if (!res.ok) {
+			toast.error('Oh no! Something went wrong')
+		} else {
+			const evt = await res.json()
+			router.push(`/events/${evt.slug}`)
+		}
 	}
 
 	const handleInputChange = e => {
@@ -37,10 +59,13 @@ export default function AddEventPage() {
 				</a>
 			</Link>
 			<h1>Add Event</h1>
-			<form onSubmit={handleSubmit}>
+
+			<ToastContainer position='top-center' draggable />
+
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<div className={styles.grid}>
 					<div>
-						<label htmlFor='name'>Event Name</label>
+						<label htmlFor='name'>Name</label>
 						<input
 							type='text'
 							id='name'
@@ -49,7 +74,75 @@ export default function AddEventPage() {
 							onChange={handleInputChange}
 						/>
 					</div>
+
+					<div>
+						<label htmlFor='performers'>Performers</label>
+						<input
+							type='text'
+							id='performers'
+							name='performers'
+							value={values.performers}
+							onChange={handleInputChange}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='venue'>Venue</label>
+						<input
+							type='text'
+							id='venue'
+							name='venue'
+							value={values.venue}
+							onChange={handleInputChange}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='address'>Address</label>
+						<input
+							type='text'
+							id='address'
+							name='address'
+							value={values.address}
+							onChange={handleInputChange}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='date'>Date</label>
+						<input
+							type='date'
+							id='date'
+							name='date'
+							value={values.name}
+							onChange={handleInputChange}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor='name'>Time</label>
+						<input
+							type='text'
+							id='time'
+							name='time'
+							value={values.time}
+							onChange={handleInputChange}
+						/>
+					</div>
 				</div>
+
+				<div>
+					<label htmlFor='description'>Description</label>
+					<textarea
+						type='text'
+						name='description'
+						id='description'
+						value={values.description}
+						onChange={handleInputChange}
+					></textarea>
+				</div>
+
+				<input type='submit' value='Add Event' className='btn' />
 			</form>
 		</Layout>
 	)
